a99c5f531ae939b008e1b87382cd37dbb895817b
Parker williams <parkster246@gmail.com>
2020-07-21 14:57:54 -0500

    MVP met but double checking

---
+++ team-builder/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Form from './Form'
+import './App.css';
+
+function App() {
+  const [addedMember, setAddedMember]=useState([]);
+  
+  return (
+    <div className="App">
+    <body>
+        <h1>Team Member List!</h1>
+       
+        <Form 
+        addedMember={addedMember} setAddedMember={setAddedMember}
+        />
+        <h4>Members</h4>
+        <div>
+            {addedMember.map(member=>(
+                <div key={member.id}>
+                    <p>Name: {member.name}</p>
+                    <p>Email: {member.email}</p>
+                    <p>Role: {member.role}</p>
+                </div>
+            ))}
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
